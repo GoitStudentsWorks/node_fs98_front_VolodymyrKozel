db1534aaf6c22a83be56e2491e72341759ee309a
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-07-29 00:17:13 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={<Loader />} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename='/node_fs98_front_VolodymyrKozel'>
           <HelmetProvider>
             <TranslationProvider>
             <App />
