0b1b816fa533ad2b31420804372c67da8e6223c0
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-07-28 21:17:01 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/node_fs98_front_VolodymyrKozel@0d84e6653db61360add0310f4070379754efa898 🚀

---
+++ assets/Logo-BWkIgZ7X.js
@@ -0,0 +1,2 @@
+import{j as s,J as t,v as c}from"./index-BySpOq-F.js";const l="_logo_jfb7y_1",n={logo:l},a=({className:o})=>s.jsx(t,{className:c(n.logo,o),to:"/",children:"AQUATRACK"});export{a as L};
+//# sourceMappingURL=Logo-BWkIgZ7X.js.map
